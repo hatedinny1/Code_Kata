1f082297f768cc8a5fa4a49615a65b8ffe4af1b4
Brian <hatedinny1@gmail.com>
2017-10-28 02:09:38 +0800

    Pass Test:{1,2,2} Substract {1} Should {2,2}

--- Code_Kata/ArrayDiffKata.cs
+++ Code_Kata/ArrayDiffKata.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ namespace Code_Kata
     {
         public int[] ArrayDiff(int[] a, int[] b)
         {
-            return new[] { 2 };
+            return a.Where(x => !b.Contains(x)).ToArray();
         }
     }
 }
--- Code_KataTests/ArrayDiffKataTests.cs
+++ Code_KataTests/ArrayDiffKataTests.cs
@@ -9,18 +9,43 @@ using FluentAssertions;
 
 namespace Code_Kata.Tests
 {
+    public class ArrayDiffInitial
+    {
+        public ArrayDiffInitial(int[] input, int[] substract, int[] expected)
+        {
+            Input = input;
+            Substract = substract;
+            Kata = new ArrayDiffKata();
+            Expected = expected;
+        }
+
+        public int[] Input { get; private set; }
+        public int[] Substract { get; private set; }
+        public ArrayDiffKata Kata { get; private set; }
+        public int[] Expected { get; private set; }
+    }
+
     [TestClass()]
     public class ArrayDiffKataTests
     {
         [TestMethod()]
         public void Input_Int_Array_1_2_Substract_Int_Array_1_Should_Be_Int_Array_2()
         {
-            var expected = new[] { 2 };
-            var input = new[] { 1,2 };
-            var substract = new[] { 1 };
-            var kata = new ArrayDiffKata();
-            var actual = kata.ArrayDiff(input, substract);
-            actual.Should().Equal(expected);
+            var arrayDiffInitial = new ArrayDiffInitial(new[] { 1, 2 }, new[] { 1 }, new[] { 2 });
+            ActualAreEqualExpected(arrayDiffInitial);
+        }
+
+        [TestMethod()]
+        public void Input_Int_Array_1_2_2_Substract_Int_Array_1_Should_Be_Int_Array_2_2()
+        {
+            var arrayDiffInitial = new ArrayDiffInitial(new[] { 1, 2, 2 }, new[] { 1 }, new[] { 2, 2 });
+            ActualAreEqualExpected(arrayDiffInitial);
+        }
+
+        private static void ActualAreEqualExpected(ArrayDiffInitial arrayDiffInitial)
+        {
+            var actual = arrayDiffInitial.Kata.ArrayDiff(arrayDiffInitial.Input, arrayDiffInitial.Substract);
+            actual.Should().Equal(arrayDiffInitial.Expected);
         }
     }
 }
