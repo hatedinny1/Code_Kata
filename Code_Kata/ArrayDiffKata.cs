d50426f30f08fb44d02bd1411587ed6a33b9cb6e
Brian <hatedinny1@gmail.com>
2017-10-28 01:55:59 +0800

    Pass Test: {1,2} Substract {1} Should {2}

---
+++ Code_Kata/ArrayDiffKata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Kata
+{
+    public class ArrayDiffKata
+    {
+        public int[] ArrayDiff(int[] a, int[] b)
+        {
+            return new[] { 2 };
+        }
+    }
+}
---
+++ Code_KataTests/ArrayDiffKataTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Code_Kata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace Code_Kata.Tests
+{
+    [TestClass()]
+    public class ArrayDiffKataTests
+    {
+        [TestMethod()]
+        public void Input_Int_Array_1_2_Substract_Int_Array_1_Should_Be_Int_Array_2()
+        {
+            var expected = new[] { 2 };
+            var input = new[] { 1,2 };
+            var substract = new[] { 1 };
+            var kata = new ArrayDiffKata();
+            var actual = kata.ArrayDiff(input, substract);
+            actual.Should().Equal(expected);
+        }
+    }
+}
