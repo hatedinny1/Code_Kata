fe1be83452a4c78ff06b7675cf05d86605a8bf1a
Brian <hatedinny1@gmail.com>
2017-11-03 01:38:15 +0800

    Pass Test: input must greater than 1

---
+++ Code_Kata/PrimeDecompKata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Kata
+{
+    public class PrimeDecompKata
+    {
+        public string Factors(int lst)
+        {
+            if (lst < 1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return "";
+        }
+    }
+}
---
+++ Code_KataTests/PrimeDecompKataTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Code_Kata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Kata.Tests
+{
+    [TestClass()]
+    public class PrimeDecompKataTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Input_N_less_than_1_should_be_throw_exception()
+        {
+            //arrange
+            var sut = new PrimeDecompKata();
+            //act
+            var actual = sut.Factors(-1);
+        }
+    }
+}
